2d850249ea84a7935799b593c14d0d02d70979c8
Jolyon Bloomfield <jolyon@mit.edu>
2020-05-23 16:16:41 -0400

    Updating C and D integrals to have integration limits based on cutoff scale

--- toy_ps.py
+++ toy_ps.py
@@ -1,17 +1,17 @@
 """
-Quick and dirty test suite for the full stack.
+Quick and dirty test suite for the full stack (uses a toy analytic power spectrum).
 """
 from stack import Model
 
 def main():
     model = Model(model_name='toy_ps', n_efolds=15, n_fields=4, mpsi=0.1, m0=10.0, verbose=True,
-                  test_ps=True, max_k=20, min_k=0, num_modes=401)
+                  test_ps=True, max_k=20, min_k=0)
     model.construct_powerspectrum()
-    # model.construct_moments()
-    # model.construct_singlebessel()
-    # model.construct_grid()
-    # model.construct_moments2()
-    # model.construct_correlations()
+    model.construct_moments()
+    model.construct_singlebessel()
+    model.construct_grid()
+    model.construct_moments2()
+    model.construct_correlations()
     
 if __name__ == '__main__':
     main()
