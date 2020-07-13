271113635b9092dc71f04285ee21185ebc4473ad
Jolyon Bloomfield <jolyon@mit.edu>
2020-07-12 23:46:52 -0400

    Implementing logic for double-bessel integrals

--- stack/integrals/common.py
+++ stack/integrals/common.py
@@ -46,7 +46,8 @@ class Integrals(Persistence):
     
     @staticmethod
     def generate_domains(min_k: float, max_k: float, peak: float,
-                         osc1: float, osc10: float, suppress: Optional[float]) -> List[Tuple[float, float]]:
+                         osc1: float, osc10: float, suppress: Optional[float],
+                         osc1_2: Optional[float] = None, osc10_2: Optional[float] = None) -> List[Tuple[float, float]]:
         """
         Generates a list of domain tuples (start_k, stop_k) that span the domain (min_k, max_k), with splits
         based on the following parameters:
@@ -57,11 +58,17 @@ class Integrals(Persistence):
         :param osc1: First oscillation k value
         :param osc10: 10th oscillation k value
         :param suppress: Suppression scale (or None if not using)
+        :param osc1_2: First oscillation k value (for double Bessels with two r values) (or None if not using)
+        :param osc10_2: 10th oscillation k value (for double Bessels with two r values) (or None if not using)
         :return: List of (min, max) tuples
         """
         possible_points = [min_k, max_k, osc1, osc10,
                            5*peak, 10*peak, 15*peak, 20*peak, 40*peak, 80*peak, 100*peak,
                            150*peak, 200*peak, 250*peak, 300*peak, 350*peak, 400*peak, 450*peak, 500*peak]
+        if osc1_2:
+            possible_points.append(osc1_2)
+        if osc10_2:
+            possible_points.append(osc10_2)
         if suppress is not None:
             # Make sure the suppression factor doesn't drop too quickly over the domain
             # by inserting suppression scale domain endpoints
--- stack/integrals/doublebessel.py
+++ stack/integrals/doublebessel.py
@@ -27,28 +27,51 @@ if TYPE_CHECKING:
 np.seterr(all='raise')
 np.seterr(under='ignore')
 
+# These are the 1st roots (half oscillation) of j_ell(x), ell ranging from 0 to 100.
+j_ell_roots_half = [3.14159265358979, 4.49340945790906, 5.76345919689455, 6.98793200050051, 8.18256145257149,
+                    9.35581211104502, 10.5128354080982, 11.6570321925211, 12.7907817119779, 13.9158226105099,
+                    15.0334693037494, 16.1447429423013, 17.2504547841260, 18.3512614959473, 19.4477031080947,
+                    20.5402298250482, 21.6292214365904, 22.7150016749722, 23.7978490341283, 24.8780050582072,
+                    25.9556807850401, 27.0310618213500, 28.1043123876971, 29.1755785769190, 30.2449910046155,
+                    31.3126669843224, 32.3787123272001, 33.4432228422462, 34.5062855955485, 35.5679799740761,
+                    36.6283785897134, 37.6875480518021, 38.7455496307433, 39.8024398307933, 40.8582708867284,
+                    41.9130911963446, 42.9669456985971, 44.0198762054710, 45.0719216942892, 46.1231185660503,
+                    47.1735008744784, 48.2231005297262, 49.2719474800637, 50.3200698743775, 51.3674942078915,
+                    52.4142454531694, 53.4603471781674, 54.5058216528616, 55.5506899457670, 56.5949720114892,
+                    57.6386867703026, 58.6818521806208, 59.7244853051161, 60.7666023711523, 61.8082188261149,
+                    62.8493493881528, 63.8900080927861, 64.9302083357819, 65.9699629126566, 67.0092840551198,
+                    68.0481834647453, 69.0866723441195, 70.1247614256945, 71.1624609985483, 72.1997809332331,
+                    73.2367307048754, 74.2733194146759, 75.3095558099414, 76.3454483027680, 77.3810049874864,
+                    78.4162336569653, 79.4511418178646, 80.4857367049176, 81.5200252943178, 82.5540143162767,
+                    83.5877102668149, 84.6211194188415, 85.6542478325752, 86.6871013653521, 87.7196856808646,
+                    88.7520062578707, 89.7840683984109, 90.8158772355643, 91.8474377407769, 92.8787547307896,
+                    93.9098328741920, 94.9406766976264, 95.9712905916646, 97.0016788163785, 98.0318455066226,
+                    99.0617946770479, 100.091530226862, 101.121055944352, 102.150375511184, 103.179492506492,
+                    104.208410410772, 105.237132609581, 106.265662397074, 107.294002979366, 108.322157477736,
+                    109.350128931692, 110.377920301894]
+
 # These are the 2nd roots (1 full oscillation) of j_ell(x), ell ranging from 0 to 100.
-j_ell_roots = [6.28318530717959, 7.72525183693771, 9.09501133047625, 10.4171185473799, 11.7049071545704,
-               12.9665301727743, 14.2073924588425, 15.4312892102684, 16.6410028815122, 17.8386431992053,
-               19.0258535361278, 20.2039426328117, 21.3739721811627, 22.5368170711198, 23.6932080374714,
-               24.8437625975863, 25.9890079764136, 27.1293984123007, 28.2653284366424, 29.3971432134409,
-               30.5251466952463, 31.6496081325095, 32.7707673241958, 33.8888388941352, 35.0040158047227,
-               36.1164722674122, 37.2263661715630, 38.3338411253205, 39.4390281814521, 40.5420473054268,
-               41.6430086311325, 42.7420135404888, 43.8391555961316, 44.9345213508109, 46.0281910527871,
-               47.1202392630483, 48.2107353974125, 49.2997442043539, 50.3873261875962, 51.4735379810525,
-               52.5584326824965, 53.6420601513630, 54.7244672752637, 55.8056982091289, 56.8857945903217,
-               57.9647957326000, 59.0427388014036, 60.1196589726104, 61.1955895766203, 62.2705622293833,
-               63.3446069517857, 64.4177522786267, 65.4900253582705, 66.5614520439272, 67.6320569774018,
-               68.7018636660544, 69.7708945536301, 70.8391710855421, 71.9067137691272, 72.9735422293379,
-               74.0396752602844, 75.1051308729959, 76.1699263397348, 77.2340782351604, 78.2976024746111,
-               79.3605143497471, 80.4228285617704, 81.4845592524212, 82.5457200329279, 83.6063240110731,
-               84.6663838165231, 85.7259116245535, 86.7849191782942, 87.8434178096031, 88.9014184586710,
-               89.9589316924505, 91.0159677219922, 92.0725364187678, 93.1286473300498, 94.1843096934139,
-               95.2395324504242, 96.2943242595563, 97.3486935084097, 98.4026483252561, 99.4561965899670,
-               100.509345944361, 101.562103802008, 102.614477357522, 103.666473595382, 104.718099298299,
-               105.769361055166, 106.820265268611, 107.870818162181, 108.921025787176, 109.970894029150,
-               111.020428614108, 112.069635114407, 113.118518954383, 114.167085415718, 115.215339642558,
-               116.263286646404]
+j_ell_roots1 = [6.28318530717959, 7.72525183693771, 9.09501133047625, 10.4171185473799, 11.7049071545704,
+                12.9665301727743, 14.2073924588425, 15.4312892102684, 16.6410028815122, 17.8386431992053,
+                19.0258535361278, 20.2039426328117, 21.3739721811627, 22.5368170711198, 23.6932080374714,
+                24.8437625975863, 25.9890079764136, 27.1293984123007, 28.2653284366424, 29.3971432134409,
+                30.5251466952463, 31.6496081325095, 32.7707673241958, 33.8888388941352, 35.0040158047227,
+                36.1164722674122, 37.2263661715630, 38.3338411253205, 39.4390281814521, 40.5420473054268,
+                41.6430086311325, 42.7420135404888, 43.8391555961316, 44.9345213508109, 46.0281910527871,
+                47.1202392630483, 48.2107353974125, 49.2997442043539, 50.3873261875962, 51.4735379810525,
+                52.5584326824965, 53.6420601513630, 54.7244672752637, 55.8056982091289, 56.8857945903217,
+                57.9647957326000, 59.0427388014036, 60.1196589726104, 61.1955895766203, 62.2705622293833,
+                63.3446069517857, 64.4177522786267, 65.4900253582705, 66.5614520439272, 67.6320569774018,
+                68.7018636660544, 69.7708945536301, 70.8391710855421, 71.9067137691272, 72.9735422293379,
+                74.0396752602844, 75.1051308729959, 76.1699263397348, 77.2340782351604, 78.2976024746111,
+                79.3605143497471, 80.4228285617704, 81.4845592524212, 82.5457200329279, 83.6063240110731,
+                84.6663838165231, 85.7259116245535, 86.7849191782942, 87.8434178096031, 88.9014184586710,
+                89.9589316924505, 91.0159677219922, 92.0725364187678, 93.1286473300498, 94.1843096934139,
+                95.2395324504242, 96.2943242595563, 97.3486935084097, 98.4026483252561, 99.4561965899670,
+                100.509345944361, 101.562103802008, 102.614477357522, 103.666473595382, 104.718099298299,
+                105.769361055166, 106.820265268611, 107.870818162181, 108.921025787176, 109.970894029150,
+                111.020428614108, 112.069635114407, 113.118518954383, 114.167085415718, 115.215339642558,
+                116.263286646404]
 
 # These are the 20th roots (10 full oscillations) of j_ell(x), ell ranging from 0 to 100.
 j_ell_roots10 = [62.8318530717959, 64.3871195905574, 65.9279415029587, 67.4552844798028, 68.9700122850280,
@@ -165,7 +188,7 @@ class DoubleBessel(Integrals):
             max_k = min(max_k, self.model.grid.sampling_cutoff * 6)
 
         # Construct the list of domains
-        osc1 = j_ell_roots[ell] / r       # 1 oscillation of j_0(x)
+        osc1 = j_ell_roots1[ell] / r       # 1 oscillation of j_0(x)
         osc10 = j_ell_roots10[ell] / r    # 10 oscillations of j_0(x)
         osc50 = j_ell_roots50[ell] / r    # 50 oscillations of j_0(x)
         domains = self.generate_domains(min_k, max_k, moments.k2peak, osc1, osc10, suppression_factor)
@@ -206,4 +229,110 @@ class DoubleBessel(Integrals):
         # Perform integration
         result = self.perform_integral(domains, selector)
 
+        return result
+
+    def compute_G(self, ell: int, r: float, rp: float, suppression: Suppression) -> float:
+        """
+        Computes the integral
+        G_ell(r, r') = 4 pi int_{k_min}^{k_max} dk k^2 P(k) j_ell(k r) j_ell(k r').
+        If r = r', then compute_E is invoked instead.
+
+        :param r: Value of r to use in the integral
+        :param rp: Value of r' to use in the integral
+        :param ell: Value of ell to use in the integral
+        :param suppression: High frequency suppression method to use
+        :return: Result of the integral
+        """
+        # Check for coincidence limit
+        if r == rp:
+            return self.compute_E(ell, r, suppression)
+
+        rmin = min(r, rp)
+        rmax = max(r, rp)
+        
+        # Treat the special case
+        if rmin == 0:
+            if ell == 0:
+                return self.model.singlebessel.computeC(rmax, suppression)
+            return 0
+
+        moments = self.model.get_moments(suppression)
+        suppression_factor = None
+        if suppression == Suppression.PEAKS:
+            raise ValueError(f'Bad suppression method: {suppression}')
+        elif suppression == Suppression.RAW:
+            suppression_factor = self.model.grid.sampling_cutoff
+
+        pk = self.model.powerspectrum
+        min_k = self.model.min_k
+        max_k = self.model.max_k
+        if suppression == suppression.SAMPLING:
+            # No need to go far out into the tail of the suppressing Gaussian
+            # At k = n k_0, the suppression is exp(-n^2/2)
+            # At n = 6, this is ~10^-8, which seems like a good place to stop
+            max_k = min(max_k, self.model.grid.sampling_cutoff * 6)
+    
+        # Construct the list of domains
+        oschalfmin = j_ell_roots_half[ell] / rmin  # half oscillation of j_0(x)
+        osc1min = j_ell_roots1[ell] / rmin  # 1 oscillation of j_0(x)
+        osc10min = j_ell_roots10[ell] / rmin  # 10 oscillations of j_0(x)
+        oschalfmax = j_ell_roots_half[ell] / rmax  # half oscillation of j_0(x)
+        osc1max = j_ell_roots1[ell] / rmax  # 1 oscillation of j_0(x)
+        osc10max = j_ell_roots10[ell] / rmax  # 10 oscillations of j_0(x)
+        # Note that the min values will be larger than the max values
+        domains = self.generate_domains(min_k, max_k, moments.k2peak, osc1min, osc10min, suppression_factor, osc1max, osc10max)
+
+        # Define integration functions
+        def f(k):
+            """Straight function to integrate"""
+            return k * k * pk(k, suppression) * spherical_jn(ell, k * rmin) * spherical_jn(ell, k * rmax)
+    
+        low_osc = self.gen_low_osc(f, "F", r)
+    
+        def hi_osc(min_k: float, max_k: float) -> float:
+            """Compute integrals for highly-oscillatory functions"""
+        
+            def func(k):
+                """Integrand weight function (excluding Levin kernel)"""
+                return k * k * pk(k, suppression)
+        
+            # Set the limits of integration
+            self.integrator.set_limits(a=min_k, b=max_k)
+        
+            # Set the amplitude function
+            self.integrator.set_amplitude(func)
+        
+            # Perform the integration
+            int_result, _ = self.integrator.integrate_K(ell=ell, alpha=rmin, beta=rmax)
+        
+            # print(f'mink={min_k}; maxk={max_k}; r={r}; l={ell}; ans={int_result};'.replace("e", "*^"))
+        
+            return int_result
+    
+        # Define selector function
+        def selector(min_k: float, max_k: float) -> Callable:
+            """Returns the function to use to perform integration on the given domain"""
+            # Choose from the following regimes
+            if max_k < osc10max:
+                # Each spherical bessel function has less than 10 oscillations
+                # Use direct integration
+                return low_osc
+            elif max_k < osc1min or max_k - min_k < 2 * pi:
+                # The slower oscillations don't have time to complete a full oscillation
+                # Treat this like a single-bessel integral with weight k**2*P(k)*j_ell(k*rmin)
+                # For ell = 0, 1, 2, use sine- and cosine-weighted integration
+                # For higher ell, use Levin integration (single-Bessel)
+                if ell == 0:
+                    raise NotImplementedError("Can't do this one just yet...")
+                elif ell == 1:
+                    raise NotImplementedError("Can't do this one just yet...")
+                elif ell == 2:
+                    raise NotImplementedError("Can't do this one just yet...")
+                raise NotImplementedError("Can't do this one just yet...")
+            # Otherwise, use Levin integration (double-Bessel)
+            return hi_osc
+    
+        # Perform integration
+        result = self.perform_integral(domains, selector)
+    
         return result
